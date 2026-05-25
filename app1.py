420c0fb0ae356dea5c6e86c74bb2c5983120dda3
ghantathanyasree10-cmyk <ghantathanyasree10@gmail.com>
2026-05-25 11:41:03 +0530

    app1.py

---
+++ app1.py
@@ -0,0 +1,27 @@
+# Simple Calculator
+
+print("Simple Calculator")
+
+# Take input from user
+num1 = float(input("Enter first number: "))
+operator = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+# Perform calculation
+if operator == "+":
+    print("Result:", num1 + num2)
+
+elif operator == "-":
+    print("Result:", num1 - num2)
+
+elif operator == "*":
+    print("Result:", num1 * num2)
+
+elif operator == "/":
+    if num2 != 0:
+        print("Result:", num1 / num2)
+    else:
+        print("Cannot divide by zero")
+
+else:
+    print("Invalid operator")
